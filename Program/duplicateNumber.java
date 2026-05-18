a2a036959f54469172a0c433e45000fcbfe238cd
yuvrajrai30 <raiyuvraj201@gmail.com>
2026-05-18 23:08:29 +0530

    18may

---
+++ Program/duplicateNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class duplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+    }
+     public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of an array");
+        int l = sc.nextInt();
+        int nums[] = new int[l];
+        for(int a = 0;a<l;a++)
+        {
+            System.out.println("Enter a number");
+            nums[a]=sc.nextInt();
+        }
+        System.out.println("duplicate number : "+findDuplicate(nums));
+
+    }
+}
